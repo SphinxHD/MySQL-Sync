a2a4d7fa73605bad67eb1ef436d7ced21b14e30a
Sphinx_HD <leo@leos-welt.net>
2021-11-10 15:27:51 +0100

    Setting up Maven and Java

---
+++ src/main/java/hd/sphinx/sync/Main.java
@@ -0,0 +1,18 @@
+package hd.sphinx.sync;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+
+    }
+}
