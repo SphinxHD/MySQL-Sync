940ce14046fbefd895d9133df27b40d84aed74f2
Sphinx_HD <leo@leos-welt.net>
2023-03-19 17:14:51 +0100

    1.2.2, 1.2.3 and 1.2.4

--- src/main/java/hd/sphinx/sync/listener/JoinListener.java
+++ src/main/java/hd/sphinx/sync/listener/JoinListener.java
@@ -2,41 +2,34 @@ package hd.sphinx.sync.listener;
 
 import hd.sphinx.sync.Main;
 import hd.sphinx.sync.mysql.ManageData;
-import hd.sphinx.sync.util.AdvancementManager;
-import hd.sphinx.sync.util.BukkitSerialization;
 import hd.sphinx.sync.util.ConfigManager;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
-import org.bukkit.potion.PotionEffect;
-
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collection;
 
 public class JoinListener implements Listener {
 
     @EventHandler
-    public void onJoin(PlayerJoinEvent e) {
-        Player p = e.getPlayer();
-        if (!ManageData.isPlayerInDB(p)) {
+    public void onJoin(PlayerJoinEvent event) {
+        Player player = event.getPlayer();
+        ManageData.loadInventory.add(player);
+        if (!ManageData.isPlayerInDB(player)) {
             if (ConfigManager.getBoolean("settings.sending.generated")) {
-                p.sendMessage(ConfigManager.getColoredString("messages.generated"));
+                player.sendMessage(ConfigManager.getColoredString("messages.generated"));
             }
-            ManageData.generatePlayer(p);
+            ManageData.generatePlayer(player);
         }
-        p.sendMessage(ConfigManager.getColoredString("messages.loading"));
-        p.getEnderChest().clear();
-        p.getInventory().clear();
-        p.setLevel(0);
+        player.sendMessage(ConfigManager.getColoredString("messages.loading"));
+        player.getEnderChest().clear();
+        player.getInventory().clear();
+        player.setLevel(0);
 
         Bukkit.getScheduler().runTaskLater(Main.main, new Runnable() {
             @Override
             public void run() {
-                ManageData.loadPlayer(p);
+                ManageData.loadPlayer(player);
             }
         }, 40l);
     }
--- src/main/java/hd/sphinx/sync/listener/QuitListener.java
+++ src/main/java/hd/sphinx/sync/listener/QuitListener.java
@@ -1,7 +1,6 @@
 package hd.sphinx.sync.listener;
 
 import hd.sphinx.sync.mysql.ManageData;
-import hd.sphinx.sync.mysql.MySQL;
 import hd.sphinx.sync.util.InventoryManager;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -11,8 +10,12 @@ import org.bukkit.event.player.PlayerQuitEvent;
 public class QuitListener implements Listener {
 
     @EventHandler
-    public void onQuit(PlayerQuitEvent e) {
-        Player p = e.getPlayer();
-        ManageData.savePlayer(p, InventoryManager.saveItems(p, p.getInventory()), InventoryManager.saveEChest(p));
+    public void onQuit(PlayerQuitEvent event) {
+        Player player = event.getPlayer();
+        if (!ManageData.loadInventory.contains(player)) {
+            ManageData.savePlayer(player, InventoryManager.saveItems(player, player.getInventory()), InventoryManager.saveEChest(player));
+        } else {
+            ManageData.loadInventory.remove(player);
+        }
     }
 }
